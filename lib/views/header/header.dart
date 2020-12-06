614c987eac3038198d15dae9bdb5c1cd16f7e367
Koobonik <kbi960130@naver.com>
2020-12-06 21:33:02 +0900

    다시 배포

--- lib/views/header/header.dart
+++ lib/views/header/header.dart
@@ -33,7 +33,7 @@ class HeaderWidget extends StatelessWidget {
                   )
               ),
               onTap: (){
-                Navigator.pushReplacement(context, MaterialPageRoute<void>(
+                Navigator.push(context, MaterialPageRoute<void>(
                     builder: (BuildContext context) {
                       return MyApp();
                     }));
